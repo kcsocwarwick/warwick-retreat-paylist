c1fd2272f4638ec32f7ad69c368e78c253cc1d8e
Dhaval Soneji <dhaval@soneji.xyz>
2021-11-09 01:49:15 +0000

    fix case

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Table from "./Components/Table";
-import Logout from "./Components/Logout";
+import Table from "./components/Table";
+import Logout from "./components/Logout";
 
 export default function Home() {
   return (
